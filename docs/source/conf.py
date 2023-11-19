87d8fe3792f8bcc9956ebd0afab402a75afa0081
Sören Zapp <dev.szapp@gmail.com>
2023-11-19 12:27:22 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'uq4rw7demo'
+copyright = '2023, Sören Zapp'
+author = 'Sören Zapp'
 
-release = '0.1'
-version = '0.1.0'
+release = uq4rw7demo.__version__
+version = uq4rw7demo.__version__
 
 # -- General configuration
 
@@ -16,12 +16,28 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
+    'sphinx.ext.mathjax',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
 ]
+
+autoclass_content = 'both'
+autodoc_member_order = 'bysource'
+
+napoleon_use_param = False
+napoleon_use_rtype = False
+napoleon_preprocess_types = True
+
+mathjax_path = ('https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/'
+                'MathJax.js?config=TeX-MML-AM_CHTML')
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    'attrs': ('https://www.attrs.org/en/stable/', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'scipy': ('https://docs.scipy.org/doc/scipy/', None),
+    'matplotlib': ('https://matplotlib.org/stable/', None),
 }
 intersphinx_disabled_domains = ['std']
 
